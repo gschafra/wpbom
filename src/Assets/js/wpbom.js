594fc75ae3e570d64af3a02f58408d253fcc1a35
Guenter Schafranek <guenter.schafranek@itx.de>
2023-03-02 15:45:13 +0100

    feat: Enabled spinner and update button disabling
    - ... after pressed and during op

--- src/Assets/js/wpbom.js
+++ src/Assets/js/wpbom.js
@@ -1,6 +1,7 @@
 jQuery( document ).ready( function( $ ) {
 	$('button.wpbom-update-cpe-update').click( function( e ) {
-		// $('button.wpbom-update-cpe-update .custom-loader').removeClass('hidden');
+		$('button.wpbom-update-cpe-update .custom-loader').removeClass('hidden');
+		$('button.wpbom-update-cpe-update').prop('disabled', true);
 		e.preventDefault();
 		var nonce = $(this).data('nonce');
 
@@ -9,8 +10,11 @@ jQuery( document ).ready( function( $ ) {
 			_ajax_nonce: nonce
 		};
 		$.post(ajaxurl, data, function(response) {
+			$('button.wpbom-update-cpe-update .custom-loader').addClass('hidden');
+			$('button.wpbom-update-cpe-update').prop('disabled', false);
 			alert("DB population done. Response: " + response);
 		});
+
 		/*var $this = $(this);
 		var $form = $this.closest('form');
 		var $spinner = $this.find('.spinner');
